a11186209341ac6a861ab18ec6ff0a939c5c978c
Akshat Pasbola <94235440+saujix@users.noreply.github.com>
2024-07-14 01:31:14 +0530

    I just burned out.

---
+++ app.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var intro = document.querySelector('.intro');
+    var offsetX = 200;
+    var offsetY = 300;
+    var mouseX = window.innerWidth / 2;
+    var mouseY = window.innerHeight / 2;
+
+    function updateIntroPosition() {
+        var newX = mouseX - offsetX + window.pageXOffset;
+        var newY = mouseY - offsetY + window.pageYOffset;
+
+        intro.style.left = newX + 'px';
+        intro.style.top = newY + 'px';
+
+        requestAnimationFrame(updateIntroPosition);
+    }
+
+    updateIntroPosition();
+
+    document.addEventListener('mousemove', function(event) {
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+    });
+
+    document.addEventListener('scroll', function() {
+        var newX = mouseX - offsetX + window.pageXOffset;
+        var newY = mouseY - offsetY + window.pageYOffset;
+
+        intro.style.left = newX + 'px';
+        intro.style.top = newY + 'px';
+    });
+});
+
+const title = document.querySelector('.title');
+let addIntervalId, removeIntervalId;
+let index = 0;
+const textToAdd = ' Pasbola';
+
+title.addEventListener('mouseover', function() {
+  clearInterval(removeIntervalId);
+  index = 0;
+  addIntervalId = setInterval(function() {
+    if (index <= textToAdd.length) {
+      title.textContent = 'Akshat' + textToAdd.substring(0, index);
+      index++;
+    } else {
+      clearInterval(addIntervalId);
+    }
+  }, 200);
+});
+
+title.addEventListener('mouseout', function() {
+  clearInterval(addIntervalId);
+  index = textToAdd.length;
+  removeIntervalId = setInterval(function() {
+    if (index >= 0) {
+      title.textContent = 'Akshat' + textToAdd.substring(0, index);
+      index--;
+    } else {
+      clearInterval(removeIntervalId);
+    }
+  }, 200);
+});
+
+

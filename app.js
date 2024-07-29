870b2ff4827dbab966c7d5dc75491a5639f79deb
Akshat Pasbola <94235440+saujix@users.noreply.github.com>
2024-07-29 21:01:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,13 +17,21 @@ requestAnimationFrame(function loop() {
   
     cursorBorder.style.transform = `translate(${cursorBorderPos.x}px, ${cursorBorderPos.y}px)`;
     requestAnimationFrame(loop);
+
   });
 
 function isHeightok() {
   if (document.querySelector('.header').style.height === '100vh'){
-    document.querySelector('.header').style.height='8vh';
-    } else if (document.querySelector('.header').style.height='8vh'){
+    document.querySelector('.header').style.height='6vh';
+    document.querySelector('.header').style.backdropFilter='blur(0px)';
+    document.querySelector('.header').style.backgroundColor='transparent';
+    document.querySelector('.nav').style.opacity='100';
+    } else if (document.querySelector('.header').style.height < '8vh'){
       document.querySelector('.header').style.height='100vh';
+      document.querySelector('.header').style.backdropFilter='blur(100px)';
+      document.querySelector('.header').style.backgroundColor='pink';
+      document.querySelector('.nav').style.opacity='0';
+
     }
 }
 
@@ -43,4 +51,19 @@ function defaultCursor() {
   document.querySelector('#cursor-border').style.top = 'calc((200px) / -2)' ;
   document.querySelector('#cursor-border').style.left = 'calc((200px) / -2)' ;
   document.querySelector('#cursor-border').style.opacity = 1 ;
+}
+
+function randomChar(){
+  const chineseCharacters = ['人', '八', '入', '山', '大', '木', '禾', '小', '牛', '太', '天'];
+  let t =  Math.round(Math.random()*10);
+  document.querySelector('.hover-bold').innerHTML = chineseCharacters[t];
+  document.querySelector('.hover-bold').style.fontSize ='50px';
+}
+
+function removeCursor(){
+  document.querySelector('#cursor').style.opacity='0';
+}
+
+function restoreCursor(){
+  document.querySelector('#cursor').style.opacity='1';
 }

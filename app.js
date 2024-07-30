f79e4f7b5e1e756ba69a0400fd7d462c207402e5
Akshat Pasbola <94235440+saujix@users.noreply.github.com>
2024-07-30 22:31:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,42 +76,101 @@ function restorePointer(){
 let t='';
 document.querySelector('.image-container').addEventListener('click', function(event) {
   if (event.target.tagName === 'IMG') {
-
-    console.log();
     if (getComputedStyle(document.querySelector(`#${event.target.id}`)).filter === 'invert(1)'){
       console.log('Clicked image class:', event.target.id);
       t = `#${event.target.id}`;
-      console.log(`#${event.target.id}`)
       document.querySelector(`#${event.target.id}`).style.filter = 'invert(0)';
       removeCursorBackground();
       restorePointer();
+      blurBackground()
+
       
       document.querySelector('#cursor-border').style.opacity = '0';
     } else if (getComputedStyle(document.querySelector(`#${event.target.id}`)).filter === 'invert(0)'){
       console.log('Clicked image class:', event.target.id);
       t = `#${event.target.id}`;
-      console.log(`#${event.target.id}`)
+      clickedOn = `#${event.target.id}`;
       document.querySelector(`#${event.target.id}`).style.filter = 'invert(1)';
       
       removePointer();
       restoreCursorBackground();
       document.querySelector('#cursor-border').style.opacity = '1';
+      blurBackground();
     }
       
 
   } else {
-    alert('nigga_what_you_tryna_click_ma_nigga?')
   }
 }
 )
+
+
+
 
 function restoreInvert(){
   if (t === ''){
   
   } else {
     document.querySelector(t).style.filter = 'invert(1)';
-    console.log(t);
   }
   
+}
+
+function boldRandChar(){
+  let t = Math.round(Math.random() * 10);
+  if (t > 6) {
+    t = t - 4;
+  }
+  document.querySelector('#b1').style.fontWeight = '100';
+  document.querySelector('#b2').style.fontWeight = '100';
+  document.querySelector('#b3').style.fontWeight = '100';
+  document.querySelector('#b4').style.fontWeight = '100';
+  document.querySelector('#b5').style.fontWeight = '100';
+  document.querySelector('#b6').style.fontWeight = '100';
+  document.querySelector(`#b${t}`).style.fontWeight = '900';
+}
+
+
+setInterval('boldRandChar()', 500)
+
+function screensaver() {
+  let t = Math.round(Math.random() * 10);
+  document.querySelector('.screensaver').style.backgroundImage = `url('img/personal/screensaver/${t}.png')`;
+}
+
+screensaver();
+
+setInterval('screensaver()', 60000);
+
+
+function blurBackground() {
+  let screensaver = document.querySelector('.screensaver');
+  let currentFilter = getComputedStyle(screensaver).filter;
+
+  if (currentFilter === 'blur(20px)') {
+    screensaver.style.filter = 'blur(0px)';
+  } else {
+    screensaver.style.filter = 'blur(20px)';
+  }
+}
+
+function blurBackground_oneway(){
+  let screensaver = document.querySelector('.screensaver');
+  let currentFilter = getComputedStyle(screensaver).filter;
+
+  if (currentFilter === 'blur(20px)') {
+    screensaver.style.filter = 'blur(0px)';
+  } else {
+  }
+}
+
+function centerEverythingVertically(){
+  console.log(`hi there ${t}`)
+
+  document.querySelector(`${t}`).style.position = 'absolute';
+  document.querySelector(`${t}`).style.top = '100px';
+  document.querySelector(`${t}`).style.right = '100px';
+
+  console.log('changed')
 }
 

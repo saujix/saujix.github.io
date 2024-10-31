91001081e9369efbb9868b038e41b2386029c93d
saujix <chutkipasbola@gmail.com>
2024-10-31 17:02:14 +0000

    added the bse, now the image i insrted doesn't cover the entire screen

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
-['red','green','blue','purple','white','lightblue','blue'].forEach(element => {
-  function colorChanger(){
-    let cell = parseInt(sessionStorage.getItem('cell'), 10) || 0;  // Ensure cell starts from 0 if undefined
-
-    if (cell < 20) {
-      cell++;
-      document.querySelector(`.x${cell}`).style.backgroundColor = element;
-      sessionStorage.setItem('cell', `${cell}`);
-      console.log(sessionStorage.getItem('cell')); // No need for second argument
-    } else if (cell == 20) {
-      cell = 1;
-      sessionStorage.setItem('cell', `${cell}`);
-      console.log(cell);
+function changeBg(){
+    let t= event.target.id;
+    if (t === "pictures"){
+        document.querySelector(".i3").classList.add("visible");  
+    } else if (t === "professional"){
+        document.querySelector(".i2").classList.add("visible");  
+        document.body.style.color="white"
+    } else if(t === "blog"){
+        document.querySelector(".i1").classList.add("visible");  
+        document.body.style.color="black"
     }
-  }
-  
-  setInterval(colorChanger, 500);
-});
+}  
+function restoreBg(){
+    let x = event.target.id; 
+    console.log(x);
+    if (x === "pictures"){
+        document.querySelector(".i3").classList.remove("visible");  
+    } else if (x === "professional"){
+        document.querySelector(".i2").classList.remove("visible");  
+        document.body.style.color="black    "
+    } else if(x === "blog"){
+        document.querySelector(".i1").classList.remove("visible");  
+        document.body.style.color="black"
+    }
+} 
+
+async function completeName() {
+    let restName = ["a", "s", "b", "o", "l", "a"];
+    
+    await Promise.all(restName.map((element, index) => 
+        new Promise(resolve => {
+            setTimeout(() => {
+                document.querySelector("#name").innerHTML += element;
+                resolve();
+            }, 100 * (Math.random() * 10));
+        })
+    ));
+    
+    if (document.querySelector("#name").innerHTML !== "Akshat P.asbola") {
+        document.querySelector("#name").innerHTML += "?";
+    }
+};
+function restoreName(){
+    let status = 1;
+    while (status === 1){
+        if (document.querySelector("#name").innerHTML=== "Akshat P."){
+            status = 0;
+        } else{
+            t = document.querySelector("#name").innerHTML;
+            document.querySelector("#name").innerHTML = t;
+        }
+    }
+
+}
